bf9c4dbef86ed665ca4a305ec8232e0e91160c70
Cyrus141 <68317222+Cyrus141@users.noreply.github.com>
2023-01-25 20:37:32 -0600

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,25 @@
+/*
+let country = "Bangladesh";
+let continent = 'Asia';
+let population = '23';
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+let javaScriptIsMoney = true;
+console.log(javaScriptIsMoney);
+console.log(typeof true);
+console.log(typeof javaScriptIsMoney);
+console.log(typeof 23);
+console.log(typeof 'ilebaba');
+console.log(typeof ilebaba);
+javaScriptIsMoney = 'YES!';
+console.log(typeof javaScriptIsMoney);
+let year;
+console.log(year);
+console.log(typeof year);
+console.log(typeof 'year');
+year = 1995;
+console.log(typeof year);
+console.log(typeof null);
---
+++ script.js
@@ -0,0 +1,18 @@
+let js = "amazing";
+console.log(30 + 98 - 7 + 33);
+console.log("Jonas");
+console.log(23);
+
+let firstName = "madwanav";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+let shorgo_angela = "SA";
+
+let myFirstJob = "Sales";
+let myCurrentjob = "Manager";
+console.log(myFirstJob);
+
+console.log(myCurrentjob);
